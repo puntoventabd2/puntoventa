892f1496fa5703af54210f44fc7ac9de98075106
puntoventabd2 <33409516+puntoventabd2@users.noreply.github.com>
2017-11-13 19:14:34 -0800

    Update Creacion de tablas .sql

--- Creacion de tablas .sql
+++ Creacion de tablas .sql
@@ -82,7 +82,7 @@ CONSTRAINT FK_VENDEDOR FOREIGN KEY(id_vendedor) REFERENCES vendedor(id_vendedor)
 
 -- Creacion de la tabla VentaDetalle --
 CREATE TABLE Venta_Detalle(
-id_ventaDetalle NUMBER(5),
+id_venta_detalle NUMBER(5),
 id_ticket NUMBER (5) NOT NULL,
 id_producto NUMBER(5) NOT NULL,
 cantidad NUMBER (10) NOT NULL,
